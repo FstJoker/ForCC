d4a0fcfbc8e2c2a967f065007b7f0021c197412b
FstJoker <76488093+FstJoker@users.noreply.github.com>
2021-03-24 23:36:11 +0800

    Add files via upload

---
+++ 3_456_M.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) {
+            return false;
+        }
+        //µ¥µ÷Õ»
+        vector<int> stk = { 0 };
+        vector<int> leftMin = { nums[0] };
+        for (int i = 0; i < n; i++) {
+            while (!stk.empty() && nums[stk.back()] <= nums[i]) {
+                stk.pop_back();
+            }
+            if (!stk.empty() && leftMin[stk.back()] < nums[i]) {
+                return true;
+            }
+            stk.push_back(i);
+            leftMin.push_back(min(leftMin.back(), nums[i]));
+        }
+        return false;
+    }
+};
