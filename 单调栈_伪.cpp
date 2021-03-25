c88d37491b0e817e52e6a8d52ab6c8b00ae14d18
FstJoker <76488093+FstJoker@users.noreply.github.com>
2021-03-25 08:01:59 +0800

    Add files via upload

---
+++ 单调栈_伪.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+#include "cstdio"
+using namespace std;
+int n, stk[10000], top;
+int main() {
+	cin >> n;
+	while (n--) {
+		int t;
+		scanf("%d", &t);
+		while (top && t <= stk[top]) top--;
+		if (!top) cout << -1 << " ";
+		else cout << stk[top] << " ";
+		stk[++top] = t;
+	}
+	return 0;
+}
